9e737371b9ddcdc532bb738d186158d4cb4b35b9
Dominic <mrdjfraser@gmail.com>
2017-05-15 22:37:46 +0100

    leaderboard view showing correctly from database

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require('sinatra')
 require('sinatra/contrib/all')
 
+require_relative('models/player')
+
+
 get '' do
   redirect '/'
+end
+
+get '/' do 
+  @players = Player.all_sorted_by_wins
+  erb(:leaderboard) 
 end
