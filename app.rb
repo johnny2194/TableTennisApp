104699fdc558134643c9592d398882029d157a92
Dominic <mrdjfraser@gmail.com>
2017-05-16 21:38:26 +0100

    refactoring just to tidy

--- app.rb
+++ app.rb
@@ -23,18 +23,17 @@ get '/game/new' do
 end
 
 post '/game/new/result' do
-@p1_id = params[:p1_id].to_i
-@p2_id = params[:p2_id].to_i
-@winner = params[:winner].to_i
+  @p1_id = params[:p1_id].to_i
+  @p2_id = params[:p2_id].to_i
+  @winner = params[:winner].to_i
 
-@new_result = Game.new({
-  'p1_id' => @p1_id,
-  'p2_id' => @p2_id,
-  'winner' => @winner
-  })
-@new_result.save
-
-redirect '/'
+  @new_result = Game.new({
+    'p1_id' => @p1_id,
+    'p2_id' => @p2_id,
+    'winner' => @winner
+    })
+  @new_result.save
+  redirect '/'
 end
 
 not_found do
@@ -47,9 +46,9 @@ get '/player/new' do
 end
 
 post '/player/new' do
-@new_player = Player.new(params)
-@new_player.save
-redirect '/'
+  @new_player = Player.new(params)
+  @new_player.save
+  redirect '/'
 end
 
 get '/player/index' do
@@ -65,23 +64,14 @@ erb(:'admin/login')
 end
 
 post '/admin/login' do
-@password = params[:password]
-
-if (@password == '1989')
-  session[:admin] = 'admin'
-  @message = "You now have Admin privileges"
-else
-  @message = "Sorry, incorrect password"
-end
-
-erb(:'admin/login_res')
-end
-
-post "/monstas" do
-  @name = params["name"]
-  store_name("names.txt", @name)
-  session[:message] = "Successfully stored the name #{@name}."
-  redirect "/monstas?name=#{@name}"
+  @password = params[:password]
+  if (@password == '1989')
+    session[:admin] = 'admin'
+    @message = "You now have Admin privileges"
+  else
+    @message = "Sorry, incorrect password"
+  end
+  erb(:'admin/login_res')
 end
 
 #PLAYER
