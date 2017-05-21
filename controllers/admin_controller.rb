7a15ec443d9f7a2c07e76c928c89923004658221
Dominic <mrdjfraser@gmail.com>
2017-05-21 21:51:09 +0100

    admin controller made and refactored, basic players index with delete functionality

---
+++ controllers/admin_controller.rb
@@ -0,0 +1,41 @@
+require('sinatra')
+require('sinatra/contrib/all')
+
+require_relative('../models/player')
+require_relative('../models/game')
+
+enable :sessions
+
+##ADMIN
+#LOG IN
+get '/admin/login' do
+  erb(:'admin/login')
+end
+
+post '/admin/login' do
+  @password = params[:password]
+  if (@password == '1989')
+    session[:admin] = 'admin'
+    @message = "You now have Admin privileges"
+  else
+    @message = "Sorry, incorrect password"
+  end
+  erb(:'admin/login_res')
+end
+
+#PLAYER
+
+get '/admin/player/index' do
+  @players = Player.all
+
+  if (session[:admin] = 'admin')
+    erb(:'admin/player/index')      
+  else
+    redirect '/admin/login'
+  end
+end
+
+post '/admin/player/:pid/delete' do
+  Player.delete(params[:pid])
+  redirect '/player/index'
+end
