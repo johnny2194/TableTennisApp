7d43e9af7cdbf2cf3c92dec76c43b6cc7337a4e4
Dominic <mrdjfraser@gmail.com>
2017-05-21 17:28:31 +0100

    avg_win_diff, avg_score_diff, best_score_diff and win_ratio as string written and tested for player

--- models/player.rb
+++ models/player.rb
@@ -33,6 +33,16 @@ class Player
     SqlRunner.run(sql3)
   end
 
+  def join_group(group)
+    sql = "INSERT INTO pl_group_join (p_id, group_id) VALUES (#{@id}, #{group.id})"
+    SqlRunner.run(sql)
+  end
+
+  def join_org(org)
+    sql = "INSERT INTO pl_org_join (p_id, org_id) VALUES (#{@id}, #{org.id})"
+    SqlRunner.run(sql)
+  end
+
   def wins()
     sql = 
     "SELECT COUNT(id) 
@@ -68,19 +78,61 @@ class Player
     return no_games_int
   end
 
-  def win_ratio()
+  def win_ratio_percentage()
     ratio = ( self.wins().to_f / self.no_games ).round(4) * 100
     return ratio
   end
 
-  def join_group(group)
-    sql = "INSERT INTO pl_group_join (p_id, group_id) VALUES (#{@id}, #{group.id})"
-    SqlRunner.run(sql)
+  def win_ratio_ratio()
+    ratio = self.wins().to_s + ":" + self.no_games.to_s
+    return ratio
   end
 
-  def join_org(org)
-    sql = "INSERT INTO pl_org_join (p_id, org_id) VALUES (#{@id}, #{org.id})"
-    SqlRunner.run(sql)
+  def avg_score_diff()
+    sql = "SELECT * FROM games WHERE p1_id=#{self.id} OR p2_id=#{self.id}"
+    games_rb = Player.map_games(sql)
+    total_diff = 0
+    total_games = games_rb.length
+    games_rb.each { |game| 
+      diff = game.p1_score - game.p2_score
+      if (diff < 0) 
+        total_diff+= (diff * -1)
+      else
+        total_diff+=diff
+      end }
+    return (total_diff.to_f/total_games).round(1)
+  end
+
+  def avg_win_diff()
+    sql = "SELECT * FROM games WHERE 
+    p1_score>p2_score AND p1_id=#{self.id} 
+    OR 
+    p2_score>p1_score AND p2_id=#{self.id}"
+    games_rb = Player.map_games(sql)
+    total_diff = 0
+    total_games = games_rb.length
+    games_rb.each { |game| 
+      diff = game.p1_score - game.p2_score
+      if (diff < 0) 
+        total_diff+= (diff * -1)
+      else
+        total_diff+=diff
+      end }
+    return (total_diff.to_f/total_games).round(1)
+  end
+
+  def best_score_diff
+    sql = "SELECT * FROM games WHERE p1_id=#{self.id} OR p2_id=#{self.id}"
+    games_rb = Player.map_games(sql)
+    best_game = 0
+    games_rb.each {|game| 
+      diff = game.p1_score - game.p2_score
+      
+      diff = (diff * -1) if (diff < 0)
+
+      best_game = diff if diff > best_game 
+    }
+    return best_game
   end
 
   ### CLASS METHODS
@@ -108,6 +160,12 @@ class Player
     players_objects_rb = players_pg.map{ |player| 
       Player.new(player)}
     return players_objects_rb
+  end
+
+  def self.map_games(sql)
+    games_pg = SqlRunner.run(sql)
+    games_rb = games_pg.map{ |game| Game.new(game)}
+    return games_rb
   end
 
 end
